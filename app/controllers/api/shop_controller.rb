8aaa3776192038c9f3f9e5f2da9d27c833ef57b5
jandird <jandird@mcmaster.ca>
2019-01-17 14:04:56 -0500

    Added create_cart and add_to_cart method

--- app/controllers/api/shop_controller.rb
+++ app/controllers/api/shop_controller.rb
@@ -1,5 +1,7 @@
 module Api
   class ShopController < ApplicationController
+
+    # Method to GET products
     def show
       # param for returning only products that are in stock
       in_stock = params['in_stock']
@@ -25,6 +27,55 @@ module Api
         @products = Product.all
       end
       render json: @products
+    end
+
+    # Method to create a new cart
+    def create_cart
+      Cart.destroy_all
+      render json: {'Success': 'New cart created!'}
+    end
+
+    # Method to add to cart
+    def add_to_cart
+      # Params to find product and how much to place in cart
+      product_name = params['name']
+      product_id = params['item_id']
+      product_quantity = params['quantity'].to_s
+
+      # Making sure necessary params are not empty
+      if (product_name.empty? && product_id.empty?) || product_quantity.empty?
+        @return = { 'Error': 'Quantity must be specified'} if product_quantity.empty?
+        @return = { 'Error': 'One of name or item_id must be specified' } if product_id.empty? && product_name.empty?
+        @return = { 'Error': 'Quantity and one of name or item_id must be specified' } if product_id.empty? && product_name.empty? && product_quantity.empty?
+      else
+        # Finding product
+        if product_name.present?
+          product_name.capitalize
+          @return = Product.find_by(name: product_name)
+        else
+          @return = Product.find_by(item_id: product_id)
+        end
+
+        if @return.nil? # Making sure product was found
+          @return = { 'Error': 'Product not found' }
+        else
+          product_quantity.to_i
+          stock = @return.quantity
+
+          if stock == 0
+            @return = { 'Error': 'Product is sold out' }
+          elsif stock < product_quantity
+            @return = { 'Error': 'Not enough stock left to meet quantity' }
+          else
+            if Cart.find_by(item_id: @return.item_id).present?
+              @return = { 'Error': 'Product already exists in cart'}
+            else
+              @return = { 'Success': product_quantity.to_s + ' x ' + @return.name + ' added to cart!' }
+            end
+          end
+        end
+      end
+      render json: @return
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_16_043731) do
+ActiveRecord::Schema.define(version: 2019_01_17_155959) do
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "item_id"
+    t.decimal "price", precision: 10, scale: 2
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "carts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.integer "item_id"
